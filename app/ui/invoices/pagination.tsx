1da0b9747c0dfbeab9e3a66376903f201c8fbe62
Munyorodee <munyoro.md95@gmail.com>
2024-08-22 01:51:33 +0200

    search param bug

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -12,7 +12,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   const currentPage = Number(searchParams.get('page')) || 1;
 
   const createPageURL = (pageNumber: number | string) => {
-    const params = new URLSearchParams(query as any); // Convert query to URLSearchParams
+    const params = new URLSearchParams(searchParams);
     params.set('page', pageNumber.toString());
     return `${pathname}?${params.toString()}`;
   };
