0bc91628eb226c67edcc695a08dbe38a194bb930
Munyorodee <munyoro.md95@gmail.com>
2024-08-22 00:05:46 +0200

    search param bug

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams } from 'next/navigation';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 export default function Search() {
